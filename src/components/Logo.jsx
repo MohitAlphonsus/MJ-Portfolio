7f99eeb7bd632343e5d46658b777753fa96a1841
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-05-12 11:05:23 +0530

    customized text logo and responsiveness added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,16 +1,40 @@
 import styled from 'styled-components';
+
 const LogoWrapper = styled.div`
-	--size: 3rem;
+	--size: 2rem;
 	font-family: var(--font-display);
+	font-size: var(--size);
+	display: flex;
+	align-items: center;
+	gap: 1rem;
+	letter-spacing: 1px;
+
+	@media (min-width: 64em) {
+		//1024px
+		--size: 2rem;
+	}
+
+	@media (min-width: 80em) {
+		//1080px
+		--size: 2.2rem;
+	}
 
 	.logo-initials {
-		font-size: var(--size);
+		background: var(--gradient);
+		background-clip: text;
+		color: transparent;
+		font-weight: 700;
 	}
 
 	.logo-divider {
+		height: calc(var(--size) + 2.5rem);
+		width: 1px;
+		rotate: 15deg;
+		background-color: var(--border-color);
 	}
 
 	.logo-name {
+		text-transform: uppercase;
 	}
 `;
 
@@ -18,7 +42,7 @@ function Logo() {
 	return (
 		<LogoWrapper>
 			<span className="logo-initials">MJ</span>
-			<span className="logo-divider">/</span>
+			<span className="logo-divider"></span>
 			<span className="logo-name">Mohit Jadhav</span>
 		</LogoWrapper>
 	);
