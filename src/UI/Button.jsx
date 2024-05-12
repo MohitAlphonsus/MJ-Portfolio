7665231555281c569b6443b944d5b8bf45004fd4
MohitAlphonsus <mohitdalphonso@gmail.com>
2024-05-12 11:47:36 +0530

    common UI components

---
+++ src/UI/Button.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const ButtonElement = styled.button`
+	font: inherit;
+	font-size: 1.8rem;
+	border: none;
+	border-radius: var(--radius-sm);
+	padding: 1.2rem 2.4rem;
+	cursor: pointer;
+`;
+
+export default function Button({ children, className, $type }) {
+	return (
+		<ButtonElement className={className} $type={$type}>
+			{children}
+		</ButtonElement>
+	);
+}
